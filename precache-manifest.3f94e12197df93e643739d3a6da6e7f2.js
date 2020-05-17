b993d24bc7767d9e474871c9cd6f5562b9197d5c
siddtheone <siddharthl@cybage.com>
2020-05-17 14:12:38 +0530

    Updates

---
+++ precache-manifest.3f94e12197df93e643739d3a6da6e7f2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70bfc68d543cbe3c65f3d98972549fd",
+    "url": "/bootcamp-testimonials/index.html"
+  },
+  {
+    "revision": "01412830a8b59bd485a6",
+    "url": "/bootcamp-testimonials/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9bc6e4cfb200d5964af5",
+    "url": "/bootcamp-testimonials/static/js/2.59b75315.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bootcamp-testimonials/static/js/2.59b75315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01412830a8b59bd485a6",
+    "url": "/bootcamp-testimonials/static/js/main.bc68cbb0.chunk.js"
+  },
+  {
+    "revision": "02529cae8f4f6a764d16",
+    "url": "/bootcamp-testimonials/static/js/runtime-main.0396139b.js"
+  },
+  {
+    "revision": "5421c364274a5e7333553851fc6b3714",
+    "url": "/bootcamp-testimonials/static/media/icon-next.5421c364.svg"
+  },
+  {
+    "revision": "0fa4e8f2cf4223608fc878a70b8357af",
+    "url": "/bootcamp-testimonials/static/media/icon-prev.0fa4e8f2.svg"
+  },
+  {
+    "revision": "bb83e5bd104d93b264cae1bdfe037a4e",
+    "url": "/bootcamp-testimonials/static/media/pattern-bg.bb83e5bd.svg"
+  },
+  {
+    "revision": "ae8c06393898e366f54e5fd28a9b3ae2",
+    "url": "/bootcamp-testimonials/static/media/pattern-curve.ae8c0639.svg"
+  },
+  {
+    "revision": "83ba950c9ccf56bd0b8871f5007e0908",
+    "url": "/bootcamp-testimonials/static/media/pattern-quotes.83ba950c.svg"
+  }
+]);
